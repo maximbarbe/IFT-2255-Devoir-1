258a5c34c58239f4a5fc3f3ed0d5b78eb4caf251
Maxim Barbe <barbe.maxim@gmail.com>
2024-11-18 12:42:23 -0500

    précisions

--- prototype2/src/main/java/org/prototype/Models/Notification.java
+++ prototype2/src/main/java/org/prototype/Models/Notification.java
@@ -3,18 +3,20 @@ package org.prototype.Models;
 import java.util.ArrayList;
 
 /**
- * Classe représentant une notification créé lors de la modification de projet et qui est recue par les résidentsé
+ * Classe représentant une notification créé lors de la modification de projet et qui est recue par les résidents
  */
 public class Notification {
     private final String titre;
     private final String description;
     private final ArrayList<String> quartiersAffectes;
+    private final ArrayList<String> ruesAffectees;    
 
 
-    public Notification(String titre, String description, ArrayList<String> quartiersAffectes) {
+    public Notification(String titre, String description, ArrayList<String> quartiersAffectes, ArrayList<String> ruesAffectees) {
         this.titre = titre;
         this.description = description;
         this.quartiersAffectes = quartiersAffectes;
+        this.ruesAffectees = ruesAffectees;
     }
 
 }
